c50ef6a8d2d6f8a71f9fb0bf1644273c3c083f6d
Jan Winter <j.winter@tu-berlin.de>
2012-09-26 14:34:27 +0200

    slight changes

--- OL490SpectrumGenerator/test.m
+++ OL490SpectrumGenerator/test.m
@@ -1,8 +1,10 @@
 
 %load 'C:\Dokumente und Einstellungen\Buschmann\Desktop\spektren\kaarst_47_direkt.mat'
-load 'C:\Dokumente und Einstellungen\Buschmann\Desktop\spektren\neuss_HPS_direkt.mat'
+load 'C:\Dokumente und Einstellungen\jaw\Desktop\spektren\neuss_HPS_direkt.mat'
+%load HPS_350_09_26.mat
+%m = measurements{1};
 s=cs2000Spectrum_2_OL490Spectrum(m);
-ol490Spec = OL490SpectrumGenerator( s, 1.0, 'C:\Dokumente und Einstellungen\Buschmann\Desktop\Development\calibrationData.mat' )
+ol490Spec = OL490SpectrumGenerator( s, 1.0, 'C:\Dokumente und Einstellungen\jaw\Desktop\Development\calibrationData.mat' )
 ol490Spec.createAdjustedSpectrum();
 
 
