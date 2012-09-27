d0a8c65fcc74108f320dd0a68e305f692fb71281
Jan Winter <j.winter@tu-berlin.de>
2012-09-27 16:51:43 +0200

    modified test mat

--- OL490SpectrumGenerator/test.m
+++ OL490SpectrumGenerator/test.m
@@ -16,3 +16,18 @@ ol490Controller.sendSpectrum( ol490Spec.ol490AdjustedSpectrum );
 
 ec = ExperimentController();
 ec.calib_background();
+
+
+ec = ExperimentController();
+ec.init();
+ec.sendFullOutputToBackgroundOL490();
+im1 = zeros( 684, 608, 3 );
+im1 (300:380, 300:380, :) = 255;
+imwrite( im1, 'im1.bmp' );
+imFile1 = fopen( 'im1.bmp' );
+imData1 = fread( imFile1, inf, 'uchar' );
+fclose( imFile1 );
+%ec.initLightCrafter();
+ec.sendImage( imData1 );
+ec.sendPositionCalibrationImage();
+ec.cleanup();
